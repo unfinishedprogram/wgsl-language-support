c5b34074578014fd4079080dc966d6badc29d62c
Noah Labrecque <noah.labrecque@gmail.com>
2023-11-04 15:01:38 -0400

    all tokens

--- src/parse/tokenizer.rs
+++ src/parse/tokenizer.rs
@@ -8,8 +8,9 @@ type RichErr<'src> = extra::Err<Rich<'src, char, Span>>;
 #[derive(Debug)]
 pub enum Token<'src> {
     Literal(Literal<'src>),
-    SyntaxToken(char),
+    SyntaxToken(&'src str),
     Ident(&'src str),
+    Trivia,
     TemplateArgsStart,
     TemplateArgsEnd,
 }
@@ -49,7 +50,56 @@ pub fn literal<'src>() -> impl Parser<'src, &'src str, Token<'src>, RichErr<'src
 }
 
 pub fn syntax_token<'src>() -> impl Parser<'src, &'src str, Token<'src>, RichErr<'src>> {
-    one_of("{}[]()@:,.;<>=*/-+%").map(Token::SyntaxToken)
+    let l3 = choice((just("<<="), just(">>=")));
+
+    let l2 = choice((
+        just("=="),
+        just("!="),
+        just("<="),
+        just(">="),
+        just("&&"),
+        just("||"),
+        just("<<"),
+        just(">>"),
+        just("->"),
+        just("=>"),
+        just("++"),
+        just("--"),
+        just("+="),
+        just("-="),
+        just("*="),
+        just("/="),
+        just("%="),
+        just("&="),
+        just("|="),
+        just("^="),
+    ));
+
+    let l1 = choice((
+        just("("),
+        just(")"),
+        just("["),
+        just("]"),
+        just("{"),
+        just("}"),
+        just(";"),
+        just("."),
+        just(","),
+        just(":"),
+        just("&"),
+        just("@"),
+        just("="),
+        just(">"),
+        just("<"),
+        just("%"),
+        just("/"),
+        just("+"),
+        just("-"),
+        just("*"),
+    ));
+
+    // Order is important here
+    choice((l3, l2, l1)).map(Token::SyntaxToken)
 }
 
 pub fn template_delimiter<'src>() -> impl Parser<'src, &'src str, Token<'src>, RichErr<'src>> {
@@ -63,22 +113,28 @@ pub fn ident<'src>() -> impl Parser<'src, &'src str, Token<'src>, RichErr<'src>>
     text::unicode::ident().map(Token::Ident)
 }
 
-pub fn tokenizer<'src>() -> impl Parser<'src, &'src str, Vec<(Token<'src>, Span)>, RichErr<'src>> {
-    let line_comment = just("//").then(none_of('\n').repeated()).padded();
-    // TODO: Make this recursive
+pub fn trivia<'src>() -> impl Parser<'src, &'src str, Token<'src>, RichErr<'src>> {
+    let line_comment = just("//")
+        .then(none_of('\n').repeated())
+        .padded()
+        .map(|_| Token::Trivia);
+
     let block_comment = {
+        // TODO: Make this recursive
         let content = any().and_is(just("/*").or(just("*/")).not()).repeated();
         content.delimited_by(just("/*"), just("*/"))
-    };
+    }
+    .map(|_| Token::Trivia);
 
-    let token = choice((literal(), template_delimiter(), syntax_token())).or(ident());
+    choice((line_comment, block_comment))
+}
+
+pub fn tokenizer<'src>() -> impl Parser<'src, &'src str, Vec<(Token<'src>, Span)>, RichErr<'src>> {
+    let token = choice((trivia(), literal(), template_delimiter(), syntax_token())).or(ident());
 
     token
         // Add spans to all tokens
         .map_with(|tok, e| (tok, e.span()))
-        // Remove comments
-        .padded_by(line_comment.repeated())
-        .padded_by(block_comment.repeated())
         // Remove whitespace
         .padded()
         // Error recovery
