56afdb9d56e5f472abf7439266260a0abdeec652
Noah Labrecque <noah.labrecque@gmail.com>
2023-12-10 10:54:15 -0500

    improve error reporting for testing

--- src/bin/tester.rs
+++ src/bin/tester.rs
@@ -1,27 +1,20 @@
-use ariadne::{Color, ColorGenerator, Fmt, Label, Report, ReportKind, Source};
+use ariadne::{ColorGenerator, Label, Report, Source};
 use wgsl_ast::front::module::{create_ast, tokenize};
 fn main() {
     let source = include_str!("test.wgsl");
     let tokens = tokenize(source);
     let ast = create_ast(&tokens);
 
-    // dbg!(&tokens);
-    // dbg!(&ast);
-
     dbg!(tokens.tokens.len());
-    // dbg!(ast);
     let mut colors = ColorGenerator::new();
-
-    let b = colors.next();
+    dbg!(&ast);
 
     for err in ast.errors {
         Report::build(ariadne::ReportKind::Error, "test.wgsl", err.span().start)
-            .with_code(3)
-            .with_message(err.message())
             .with_label(
                 Label::new(("test.wgsl", err.span().into_range()))
-                    .with_message("Unexpected token")
-                    .with_color(b),
+                    .with_message(err.message())
+                    .with_color(colors.next()),
             )
             .finish()
             .print(("test.wgsl", Source::from(include_str!("test.wgsl"))))
